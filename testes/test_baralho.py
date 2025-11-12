ea40a080201051314d6526770dac5e887c4581cb
fennerspohr <isadorafennerspohr@gmail.com>
2025-11-12 01:20:10 -0300

    Testes da classe Baralho

---
+++ testes/test_baralho.py
@@ -0,0 +1,53 @@
+"""Testes de métodos da classe BAralho"""
+
+import sys
+import random
+import pytest
+sys.path.append('../qs-testes-truco-25-2')
+
+from truco.baralho import Baralho
+
+@pytest.fixture
+def baralho():
+    return Baralho()
+
+def test_criar_baralho(baralho):
+    """Testa se o baralho é criado com naipes e valores válidos"""
+
+    for n in range(0, 40):
+        assert baralho.cartas[n].naipe in ["ESPADAS", "OUROS", "COPAS", "BASTOS"]
+        assert (1 <= baralho.cartas[n].numero < 8) or (10 <= baralho.cartas[n].numero < 13)
+
+def test_embaralhar(baralho):
+    """Utiliza uma seed específica para testar se o baralho está sendo embaralhado"""
+    seed = 0xBEEF
+
+    random.seed(seed)
+    expected = random.shuffle(Baralho().cartas)
+
+    assert baralho.embaralhar() == expected
+
+def test_retirar_carta(baralho):
+    """Testa se a carta retornada é a última e se o baralho diminui"""
+
+    carta = baralho.cartas[39]
+    assert baralho.retirar_carta() == carta
+    assert len(baralho.cartas) == 39
+
+def test_resetar(baralho):
+    """Testa se o baralho fica zerado depois de resetar"""
+
+    baralho.resetar()
+    assert baralho.vira == []
+    assert baralho.manilhas == []
+    assert baralho.cartas == []
+
+def test_printar_baralho(baralho, capsys):
+    """Verifica se a exibição do baralho está correta"""
+    
+    expected = ""
+    for c in baralho.cartas:
+        expected += f"[] {c.numero} de {c.naipe}\n"
+    baralho.printar_baralho()
+    captured = capsys.readouterr()
+    assert captured.out == expected
